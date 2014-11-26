cc6c8b1d92a7dfdca43039d5b61a3a3c55209312
Rich Ramsland <rramsland@gmail.com>
2014-11-26 17:32:32 -0500

    Accordion stable build

--- js/main.js
+++ js/main.js
@@ -4,15 +4,34 @@ $(document).ready(function(){
 		$(".nav-main").slideToggle();
 	});
 
-	//$(".panel-title").click(function(){});
+	
 	$('.panel-content').hide();
+	$(".panel-title").click(function(){
 
-	$(".panels").accordion(); //{collapsible: true}
+		$('.panel-content').slideUp();
+		if ($(this).next().css('display')!='none'){
+			//alert('Yarp');
+			$(this).next().slideUp();
+		}
+		else{
+			//alert('Narp');
+			$(this).next().slideDown();
+		}
+		
+
+		//$(this).next().slideToggle();
+	});
+
+	//$(".accordion").accordion();
 	
 
 	$(window).resize(function() {
         if( $(window).width() > 480 ) {  
         $('.nav-main').show();
+        }
+
+        if( $(window).width() < 480 ) {  
+        $('.nav-main').hide();
         }
     });
 
